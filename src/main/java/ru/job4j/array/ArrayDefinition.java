83c4ca96f1792ac156fda775c8cc3c4bdcf2478d
Den <den.mitskevich@gmail.com>
2025-01-23 18:33:35 +0300

    6.0.3. Заполнение массива. [#226300 #126498]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,10 +4,21 @@ public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
+        String[] names = new String[4];
         float[] prices = new float[40];
+
+        names[0] = "Ivan Safronov";
+        names[1] = "Mikhail Ivanov";
+        names[2] = "Sergei Sergeev";
+        names[3] = "Mikhail Lukin";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
