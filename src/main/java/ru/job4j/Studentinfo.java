763d3d7a3ba50dd65a320bc0857b19f15ff369c6
MickDS_PC <den.mitskevich@gmail.com>
2025-09-22 20:21:18 +0300

    4.1. Разбор первой программы. [#159033 #126530]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Mitskevich Denis");
+        System.out.println("12.09.1988");
+    }
+}
